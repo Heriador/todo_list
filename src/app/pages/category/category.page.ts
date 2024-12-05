0331694c05496e06146ce93502adda7754f6ad64
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-12-05 13:15:07 -0500

    feat: save categories in firebase database

--- src/app/pages/category/category.page.ts
+++ src/app/pages/category/category.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Category } from 'src/app/interfaces/category.interface';
+import { User } from 'src/app/interfaces/user.interface';
 import { CategoryService } from 'src/app/services/category/category.service';
+import { UtilsService } from 'src/app/services/utils/utils.service';
 
 @Component({
   selector: 'app-category',
@@ -10,7 +12,7 @@ import { CategoryService } from 'src/app/services/category/category.service';
 })
 export class CategoryPage implements OnInit {
 
-  public categories: Category[];
+  public categories: Category[] = [];
 
   public createCategoryForm: FormGroup
   public updateCategoryForm: FormGroup
@@ -21,7 +23,8 @@ export class CategoryPage implements OnInit {
 
   constructor(
     private readonly formBuilder: FormBuilder,
-    private readonly categoryService: CategoryService
+    private readonly categoryService: CategoryService,
+    private readonly utilsService: UtilsService
   ) {
 
     this.createCategoryForm = this.formBuilder.group({
@@ -34,11 +37,13 @@ export class CategoryPage implements OnInit {
       description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(80)]]
     })
 
-    this.categories = this.categoryService.getCategories();
-
   }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.getCategories();
   }
 
   get nameCreate(): FormControl{
@@ -57,38 +62,114 @@ export class CategoryPage implements OnInit {
     return this.updateCategoryForm.controls['description'] as FormControl;
   }
 
-  addCategory(){
-    if(this.createCategoryForm.valid){
-      let category: Category = {
-        id: this.categories.length + 1,
-        ...this.createCategoryForm.value
+  getErrorMessage(control: FormControl): string{
+
+    if(control.touched && control.errors){
+      if(control.errors["required"]){
+        return 'Campo requerido';
       }
-      this.categoryService.addCategory(category);
-      this.categories = this.categoryService.getCategories();
-      this.createCategoryForm.reset();
-      this.closeModal();
+      if(control.errors["minlength"]){
+        return `Campo debe tener al menos ${control.errors["minlength"].requiredLength} caracteres`;
+      }
+      if(control.errors["maxlength"]){
+        return `Campo debe tener menos de ${control.errors["maxlength"].requiredLength} caracteres`;
+      }
     }
+
+
+    return '';
+  }
+
+  addCategory(){
+    if(this.createCategoryForm.invalid){
+      this.createCategoryForm.markAllAsTouched();
+      
+    }
+
+    this.categoryService.addCategory(this.createCategoryForm.value).then((response) => {
+      console.log(response);
+      this.utilsService.presentToast({
+        message: 'Categoria creada',
+        duration: 2000,
+        color: 'success',
+        position: 'top'
+      })
+      this.createCategoryForm.reset({
+        name: '',
+        description: ''
+      });
+      this.closeModal()
+    })
+    .catch((error) => {
+      this.utilsService.presentToast({
+        message: 'Error al crear la categoria',
+        duration: 2000,
+        color: 'danger',
+        position: 'top'
+      })
+    });
+    ;
+  }
+
+  async getCategories(){
+
+    const loading = await this.utilsService.loading();
+    await loading.present();
+
+    let sub = this.categoryService.getCategories().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.categories = response;
+        sub.unsubscribe();
+      },
+      error: (error) => {
+        console.error(error);
+        this.utilsService.presentToast({
+          message: 'Error al obtener las categorias',
+          duration: 2000,
+          color: 'danger',
+          position: 'top'
+        })
+      }
+    })
+
+    sub.add(() => {
+      loading.dismiss();
+    })
   }
 
   deleteCategory(category: Category){
     this.categoryService.deleteCategory(category);
-    this.categories = this.categoryService.getCategories();
   }
 
   updateCategory(){
-    if(this.updateCategoryForm.valid){
-      let updatedCategory: Category = {
-        id: this.selectedCategory?.id,
-        ...this.updateCategoryForm.value
-      }
-      this.categoryService.updateCategory(updatedCategory);
-      this.categories = this.categoryService.getCategories();
-      this.updateCategoryForm.reset({
-        name: '',
-        description: ''
-      });
-      this.closeUpdateModal();
+    if(this.updateCategoryForm.invalid){
+      this.updateCategoryForm.markAllAsTouched();
     }
+
+    this.categoryService.updateCategory(this.updateCategoryForm.value)
+      .then(() => {
+        this.utilsService.presentToast({
+          message: 'Categoria actualizada',
+          duration: 2000,
+          color: 'success',
+          position: 'top'
+        })
+        this.updateCategoryForm.reset({
+          name: '',
+          description: ''
+        });
+        this.closeUpdateModal();
+        this.getCategories();
+      })
+      .catch(() => {
+        this.utilsService.presentToast({
+          message: 'Error al actualizar la categoria',
+          duration: 2000,
+          color: 'danger',
+          position: 'top'
+        })
+      });
   }
 
   openModal(){
